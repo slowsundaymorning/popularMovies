f2469ef6e8ce0890ec508f7d7f142ca04199d7eb
slowsundaymorning <slowsundaymorning@gmail.com>
2015-08-12 01:43:23 -0400

    got size working

--- app/src/main/java/com/udacity/nano/movie/ImageAdapter.java
+++ app/src/main/java/com/udacity/nano/movie/ImageAdapter.java
@@ -1,13 +1,16 @@
 package com.udacity.nano.movie;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,19 +46,40 @@ public class ImageAdapter extends BaseAdapter {
 
     private final static String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w185/%s";
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if(convertView == null) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ImageView imageView = (ImageView) convertView;
+        if(convertView == null)
             imageView = new ImageView(mContext);
-        } else {
-            imageView = (ImageView) convertView;
-        }
         String url = getItem(position);
         Log.d(LOG_TAG, String.format("Poster Url %d : %s", position, url));
-
+        final int targetWidth = parent.getWidth()/2;
+        Transformation transformation = new Transformation() {
+            @Override
+            public Bitmap transform(Bitmap source) {
+                double aspectRatio = (double) source.getHeight() / (double) source.getWidth();
+                int targetHeight = (int) (targetWidth * aspectRatio);
+                Log.d(LOG_TAG, String.format("source width: %d, height: %d, target width : %d, height: %d",
+                        source.getHeight() , source.getWidth(),
+                        targetWidth, targetHeight ));
+                Bitmap result = Bitmap.createScaledBitmap(source, targetWidth, targetHeight, false);
+                if (result != source) {
+                    // Same bitmap is returned if sizes are the same
+                    source.recycle();
+                }
+                return result;
+            }
+            @Override
+            public String key() {
+                return "transformation" + " desiredWidth";
+            }
+        };
         Picasso.with(mContext).
-                load(String.format(BASE_POSTER_URL,getItem(position))).
+                load(String.format(BASE_POSTER_URL, getItem(position))).
+                transform(transformation).
+//                resize(500,800).
+//                centerInside().
                 into(imageView);
+
         return imageView;
     }
 
